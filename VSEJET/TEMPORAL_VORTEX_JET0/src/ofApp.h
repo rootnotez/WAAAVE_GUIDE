7c0215a124a4d7c6fc16381fa305e0ec01b64499
rootnotez <johnpublicrss@gmail.com>
2025-01-22 13:09:57 -0500

    upload TEMPORAL_VORTEX_JET0

---
+++ VSEJET/TEMPORAL_VORTEX_JET0/src/ofApp.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxMidi.h"
+
+#define ROOT_THREE 1.73205080757
+
+
+
+class ofApp : public ofBaseApp, public ofxMidiListener {
+	
+public:
+	
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	
+	float time_osc(float frequency,int shape,int osc_id,int x, int y);
+    ofVec2f rotation(ofVec2f coord,float theta);
+	
+	void newMidiMessage(ofxMidiMessage& eventArgs);
+	void midiSetup();
+	ofxMidiIn midiIn;
+	std::vector<ofxMidiMessage> midiMessages;
+	std::size_t maxMessages = 10; //< max number of messages to keep track of
+	void midibiz();
+	
+	void allocateAndDeclareSundries();
+	
+	void time_displace();
+
+	ofShader shader_blur;
+	void inputSetup();
+    ofVideoGrabber input1;
+    void inputUpdate();
+    
+    void p_lockSetup();
+    void p_lockUpdate();
+    
+    //noise oscillator
+    ofImage osc0_noise_image;
+    ofImage osc1_noise_image;
+    ofImage perlin_noise(float theta, float resolution, ofImage noise_image);
+    
+};
